7d1d8a4897e752a90ebf67ebf01ed92b3b21d2db
Nutcase <30822034+Robert-Grundstrom@users.noreply.github.com>
2019-02-14 23:29:53 +0100

    Update repo.sh

--- repo.sh
+++ repo.sh
@@ -1,6 +1,5 @@
 DISTRIB=$1
 VERSION=$2
-MNTPATH="/var/www/html/$DISTRIB-$VERSION"
 MNTDEST="/var/www/html"
 DKIMAGE="$DISTRIB:repo"
 DKNAME="$DISTRIB-docker"
@@ -8,25 +7,52 @@ CENTOS_SALTREPO="https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el$VER
 UBUNTU_SALTREPO="https://repo.saltstack.com/apt/ubuntu/$VERSION/latest"
 WORKDIR=$(pwd)
 
+# ########################### #
+# Check script compability.   #
+# ########################### #
+function check_version () {
+case "$DISTRIB.$VERSION" in
+  centos.6)
+    echo "OK" > /dev/null
+  ;;
+  centos.7)
+    echo "OK" > /dev/null
+  ;;
+  ubuntu.14.04)
+    DISTNAME="thrusty"
+  ;;
+  ubuntu.16.04)
+    DISTNAME="xenial"
+  ;;
+  ubuntu.18.04)
+    DISTNAME="bionic"
+  ;;
+  *)
+cat << EOF
+Warning unknown distribution or version!
+Supported distributions and versions for
+this script is:
+- ubuntu 14.04
+- ubuntu 16.04
+- ubuntu 18.04
+- centos 6
+- centos 7
+
+command syntax:
+./create-repo <distribution> <version>
+EOF
+exit 1
+  ;;
+esac
+}
+
 # ############################## #
 # Create docker file for CentOS. #
 # ############################## #
 function centos_docker () {
-case $VERSION in
-  6)
-    continue
-  ;;
-  7)
-    continue
-  ;;
-  *)
-    echo "Warning unknown version!"
-    echo "Avaible versions for centos is 6, 7"
-    exit 1
-  ;;
-esac
-
-cat <<EOT>> $WORKDIR/$DKNAME
+MNTPATH="/var/www/html/$DISTRIB/$VERSION"
+mkdir -p $MNTPATH 1&2> /dev/null
+cat << EOT >> $WORKDIR/$DKNAME
 FROM $DISTRIB:$VERSION
 RUN yum install -y yum-utils epel-release createrepo
 RUN yum install -y $CENTOS_SALTREPO
@@ -41,32 +67,20 @@ EOT
 function centos_repo () {
    REPOID=$1
    docker exec $DKNAME reposync -g -l -d -m --repoid=$REPOID --newest-only --download-metadata --download_path=$MNTDEST/
-   docker exec $DKNAME rm -f $(/usr/bin/repomanage -ock3 $MNTDEST/$REPOID/)
-   docker exec $DKNAME createrepo -g $MNTDEST/base/comps.xml $MNTDEST/$REPOID/
+}
+
+function centos_clean_build () {
+    docker exec $DKNAME /bin/sh -c 'rm -f $(/usr/bin/repomanage -ock 3 '$MNTDEST')'
+    docker exec $DKNAME createrepo -g $MNTDEST/base/comps.xml $MNTDEST
+    docker exec $DKNAME mkdir -p $MNTDEST/rpm-gpg/
+    docker exec $DKNAME cp -r /etc/pki/rpm-gpg/* $MNTDEST/rpm-gpg/
 }
 
 # ############################## #
 # Create docker file for Ubuntu. #
 # ############################## #
 function ubuntu_docker () {
-case $VERSION in
-  14.04)
-    DISTNAME="thrusty"
-  ;;
-  16.04)
-    DISTNAME="xenial"
-  ;;
-  18.04)
-    DISTNAME="bionic"
-  ;;
-  *)
-    echo "Warning unknown version!"
-    echo "Avaible versions for ubuntu is 14.04, 16.04, 18.04"
-    exit 1
-  ;;
-esac
-
-cat <<EOT>> $WORKDIR/$DKNAME
+cat << EOT >> $WORKDIR/$DKNAME
 FROM $DISTRIB:$VERSION
 RUN echo "deb $UBUNTU_SALTREPO $DISTNAME main" > /etc/apt/source.list.d/saltstack-latest
 ADD $UBUNTU_SALTREPO/SALTSTACK-GPG-KEY.pub /tmp/SALTSTACK-GPG-KEY.pub
@@ -80,10 +94,16 @@ EOT
 # ################### #
 # Script starts here. #
 # ################### #
-cd $WORKDIR
-$DISTRIB"_docker"
 
+# Move to our work directory and check syntax.
+cd $WORKDIR
+check_version
+
+# Build the docker file for our docker image.
+$DISTRIB"_docker"
 docker build -t $DKIMAGE -f $DKNAME .
+
+# Run our priviously build docker container.
 docker run -v $MNTPATH:$MNTDEST --name $DKNAME -t -d $DKIMAGE
 
 # Create our repositorys.
@@ -98,11 +118,7 @@ case $DISTRIB in
     centos_repo updates
     centos_repo epel
     centos_repo salt-latest
-  ;;
-  *)
-    echo "Warning unknown distribution!"
-    echo "Avaible destributions is centos, ubuntu"
-    exit 1
+    centos_clean_build
   ;;
 esac
 
