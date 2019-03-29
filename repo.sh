a2e1a026251a1e38f622d6bd7f48ca39aff6118e
Nutcase <30822034+Robert-Grundstrom@users.noreply.github.com>
2019-03-29 11:25:51 +0100

    Update repo.sh

--- repo.sh
+++ repo.sh
@@ -1,43 +1,35 @@
 DISTRIB=$1
 VERSION=$2
 DKIMAGE="$DISTRIB:repo"
-DKNAME="$DISTRIB-docker"
-WORKDIR=$(pwd)
+DKNAME="'$DISTRIB''$VERSION'-docker"
+WORKDIR="/opt/os/scripts"
 
 # ########################### #
 # Check script compability.   #
 # ########################### #
-function check_version () {
+function check_comp () {
 case "$DISTRIB.$VERSION" in
   centos.[6-7])
     CENTOS_SALTREPO="https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el$VERSION.noarch.rpm"
     CENTOS_PERCONA="https://repo.percona.com/yum/percona-release-latest.noarch.rpm"
-    MNTPATH="/var/www/html/"
+    MNTPATH="/opt/os/mirror/"
     MNTDEST="/var/www/html"
   ;;
-  ubuntu.14.04)
-    DISTNAME="thrusty"
-    UBUNTU_SALTREPO="https://repo.saltstack.com/apt/ubuntu/$VERSION/latest"
-    MNTDEST="/var/www/html"
-  ;;
-  ubuntu.16.04)
-    DISTNAME="xenial"
-    UBUNTU_SALTREPO="https://repo.saltstack.com/apt/ubuntu/$VERSION/latest"
-    MNTDEST="/var/www/html"
-  ;;
-  ubuntu.18.04)
+
+  ubuntu.all)
     DISTNAME="bionic"
-    UBUNTU_SALTREPO="https://repo.saltstack.com/apt/ubuntu/$VERSION/latest"
-    MNTDEST="/var/www/html"
+    VERSION="18.04"
+    UBUNTU_SALTREPO="https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest"
+    MNTDEST="/var/spool/apt-mirror"
+    MNTPATH="/opt/os/mirror/ubuntu"
   ;;
+
   *)
 cat << EOF
 Warning unknown distribution or version!
 Supported distributions and versions for
 this script is:
-- ubuntu 14.04
-- ubuntu 16.04
-- ubuntu 18.04
+- ubuntu all
 - centos 6
 - centos 7
 command syntax:
@@ -52,13 +44,10 @@ esac
 # Create docker file for CentOS. #
 # ############################## #
 function centos_docker () {
-mkdir -p $MNTPATH
-cat << EOT >> $WORKDIR/$DKNAME
+cat << EOT > $WORKDIR/$DKNAME
 FROM $DISTRIB:$VERSION
-RUN yum install -y yum-utils epel-release createrepo
-RUN yum install -y $CENTOS_SALTREPO $CENTOS_PERCONA
-RUN rpm --import /etc/pki/rpm-gpg/*
-RUN yum -y update
+RUN yum install -y yum-utils rsync epel-release createrepo $CENTOS_SALTREPO $CENTOS_PERCONA \
+&& rpm --import /etc/pki/rpm-gpg/* && yum clean all && yum -y update
 EOT
 }
 
@@ -69,62 +58,92 @@ function centos_repo () {
    REPOID=$1
    DPATH=$2
    docker exec $DKNAME reposync -g -l -d -m --repoid=$REPOID --newest-only --download-metadata --download_path=$DPATH
-   docker exec $DKNAME /bin/sh -c 'rm -f $(/usr/bin/repomanage -ock 3 '$MNTDEST/$REPOID')'
-   docker exec $DKNAME createrepo -g $MNTDEST/base/comps.xml $MNTDEST/$REPOID
+   echo ""
+   docker exec $DKNAME /bin/sh -c 'rm -f $(/usr/bin/repomanage -ock 5 '$DPATH/$REPOID') 2>&1 /dev/null'
+   touch $DPATH/$REPOID/comps.xml
+   docker exec $DKNAME createrepo -g comps.xml $DPATH/$REPOID
 }
+
 
 # ############################## #
 # Create docker file for Ubuntu. #
 # ############################## #
 function ubuntu_docker () {
-cat << EOT >> $WORKDIR/$DKNAME
+cat << EOT > $WORKDIR/$DKNAME
 FROM $DISTRIB:$VERSION
-RUN echo "deb $UBUNTU_SALTREPO $DISTNAME main" > /etc/apt/source.list.d/saltstack-latest
+RUN apt -y update && apt -y install apt-utils apt-mirror gnupg wget vim && apt -y upgrade
 ADD $UBUNTU_SALTREPO/SALTSTACK-GPG-KEY.pub /tmp/SALTSTACK-GPG-KEY.pub
 RUN apt-key add /tmp/SALTSTACK-GPG-KEY.pub
-RUN apt update
-RUN apt -y intstall apt-mirror
-RUN apt -y upgrade
+COPY $WORKDIR/ubuntu-mirror.list /etc/apt/mirror.list
 EOT
 }
+
+
+# ############################## #
+# Timer function.                #
+# ############################## #
+function take_time  () {
+if (( $SECONDS > 3600 )) ; then
+    let "hours=SECONDS/3600"
+    let "minutes=(SECONDS%3600)/60"
+    let "seconds=(SECONDS%3600)%60"
+    echo "Completed in $hours hour(s), $minutes minute(s) and $seconds second(s)"
+elif (( $SECONDS > 60 )) ; then
+    let "minutes=(SECONDS%3600)/60"
+    let "seconds=(SECONDS%3600)%60"
+    echo "Completed in $minutes minute(s) and $seconds second(s)"
+else
+    echo "Completed in $SECONDS seconds"
+fi
+}
+
 
 # ################### #
 # Script starts here. #
 # ################### #
-
-# Move to our work directory and check syntax
-# and set variables.
 cd $WORKDIR
-check_version
-
-# Build the docker file for our docker image.
+check_comp
 $DISTRIB"_docker"
+
 docker build -t $DKIMAGE -f $DKNAME .
 docker run -v $MNTPATH:$MNTDEST --name $DKNAME -t -d $DKIMAGE
 
 # Create our repositorys.
 case $DISTRIB in
-  ubuntu)
-# WIP e.g. insert code here!
-  ;;
-  centos)
-    DPATH="/$MNTDEST/$DISTRIB/$VERSION/"
-#    centos_repo base $DPATH
-#    centos_repo centosplus $DPATH
-#    centos_repo extras $DPATH
-#    centos_repo updates $DPATH
-#    centos_repo epel $DPATH
 
-    DPATH="/$MNTDEST/$DISTRIB/"
-    centos_repo salt-latest $DPATH/salt
-    centos_repo percona-release-x86_64 $DPATH/percona
-    centos_repo percona-release-noarch $DPATH/percona
+  ubuntu)
+    SECONDS=0
+    docker exec $DKNAME apt-mirror
+    take_time
+  ;;
+
+  centos)
+    SECONDS=0
+    DPATH="/var/www/html/centos/$VERSION"
+    mkdir -p $DPATH
+
+    # Download base packages
+    centos_repo base $DPATH
+    centos_repo centosplus $DPATH
+    centos_repo extras $DPATH
+    centos_repo updates $DPATH
+    centos_repo epel $DPATH
+
+    # Download percona repo
+#    centos_repo percona-release-noarch $DPATH
+#    centos_repo percona-release-x86_64 $DPATH
+
+    # Download salt-stack repo.
+    centos_repo salt-latest $DPATH
+
+    # Copy GPG keys for the repositorys.
+    docker exec $DKNAME /bin/sh -c 'mkdir -p '$DPATH'/GPG-keys/'
+    docker exec $DKNAME /bin/sh -c 'rsync -avp /etc/pki/rpm-gpg/* '$DPATH'/GPG-keys/'
+    take_time
   ;;
 esac
 
-# Stop the docker.
-docker stop $DKNAME
-
 # Clean up dockers and files.
+docker stop $DKNAME
 docker system prune -f --all
 rm -f $WORKDIR/$DKNAME
